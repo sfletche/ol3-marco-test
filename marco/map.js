b84f3e62b315c77c4c9d467f77abb13512df2f58
sfletche <sfletche@gmail.com>
2014-11-02 18:48:22 -0800

    formatting the javascript a bit

--- marco/map.js
+++ marco/map.js
@@ -1,12 +1,11 @@
 
+// other ESRI Basemaps to explore...
+// World_Street_Map, World_Topo_Map, World_Imagery, NatGeo_World_Map, World_Light_Gray_Base
 var oceans = new ol.layer.Tile({
-    source: new ol.source.XYZ({
-        // ESRI Basemaps to explore...
-        // World_Street_Map, World_Topo_Map, World_Imagery, NatGeo_World_Map, World_Light_Gray_Base
+    source: new ol.source.XYZ({        
         url: 'http://server.arcgisonline.com/arcgis/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}'
     })
 });
-
 var watercolor = new ol.layer.Tile({
     source: new ol.source.XYZ({
         // ESRI Basemaps to explore...
@@ -14,7 +13,6 @@ var watercolor = new ol.layer.Tile({
         url: 'http://tile.stamen.com/watercolor/{z}/{x}/{y}.png'        
     })
 });
-
 var natgeo = new ol.layer.Tile({
     source: new ol.source.XYZ({
         // ESRI Basemaps to explore...
@@ -22,26 +20,25 @@ var natgeo = new ol.layer.Tile({
         url: 'http://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}'
     })
 });
-
-var basemaps = {'oceans': oceans, 'watercolor': watercolor, 'natgeo': natgeo},
-    basemap = oceans;
+var basemaps = {'oceans': oceans, 'watercolor': watercolor, 'natgeo': natgeo};
+var basemap = oceans;
 
 var map = new ol.Map({
-  target: 'map',
-  layers: [
-    basemap
-  ],
-  renderer: exampleNS.getRendererFromQueryString(),
-  target: document.getElementById('map'),
-  controls: ol.control.defaults({
-    attributionOptions: /** @type {olx.control.AttributionOptions} */ ({
-      collapsible: false
+    target: 'map',
+    layers: [
+        basemap
+    ],
+    renderer: exampleNS.getRendererFromQueryString(),
+    target: document.getElementById('map'),
+    controls: ol.control.defaults({
+        attributionOptions: /** @type {olx.control.AttributionOptions} */ ({
+            collapsible: false
+        })
+    }),
+    view: new ol.View({
+        center: [-8139259.370296092, 4705772.670696784],
+        zoom: 6
     })
-  }),
-  view: new ol.View({
-    center: [-8139259.370296092, 4705772.670696784],
-    zoom: 6
-  })
 });
 
 app = {};
@@ -53,101 +50,64 @@ map.currentBasemap = basemap;
 
 var styleCache = {};
 var canyons = new ol.layer.Vector({
-  source: new ol.source.GeoJSON({
-    projection: 'EPSG:3857',
-    url: 'data/geojson/canyons.json'
-  }),
-  style: function(feature, resolution) {
-    var text = resolution < 5000 ? feature.get('name') : '';
-    if (!styleCache[text]) {
-      styleCache[text] = [new ol.style.Style({
-        fill: new ol.style.Fill({
-          color: 'rgba(255, 255, 255, 0.6)'
-        }),
-        stroke: new ol.style.Stroke({
-          color: '#319FD3',
-          width: 1
-        }),
-        text: new ol.style.Text({
-          font: '12px Calibri,sans-serif',
-          text: text,
-          fill: new ol.style.Fill({
-            color: '#000'
-          }),
-          stroke: new ol.style.Stroke({
-            color: '#fff',
-            width: 3
-          })
-        })
-      })];
-    }
-    return styleCache[text];
-  }
+    source: new ol.source.GeoJSON({
+        projection: 'EPSG:3857',
+        url: 'data/geojson/canyons.json'
+    })
 });
 map.layers['canyons'] = canyons;
 
 var highlightStyleCache = {};
 var featureOverlay = new ol.FeatureOverlay({
-  map: map,
-  style: function(feature, resolution) {
-    var text = resolution < 5000 ? feature.get('name') : '';
-    if (!highlightStyleCache[text]) {
-      highlightStyleCache[text] = [new ol.style.Style({
-        stroke: new ol.style.Stroke({
-          color: '#f00',
-          width: 1
-        }),
-        fill: new ol.style.Fill({
-          color: 'rgba(255,0,0,0.1)'
-        }),
-        text: new ol.style.Text({
-          font: '12px Calibri,sans-serif',
-          text: text,
-          fill: new ol.style.Fill({
-            color: '#000'
-          }),
-          stroke: new ol.style.Stroke({
-            color: '#f00',
-            width: 3
-          })
-        })
-      })];
+    map: map,
+    style: function(feature, resolution) {
+        var text = resolution < 5000 ? feature.get('NAME') : '';
+        if (!highlightStyleCache[text]) {
+            highlightStyleCache[text] = [new ol.style.Style({
+                stroke: new ol.style.Stroke({
+                    color: '#00f',
+                    width: 1
+                }),
+                fill: new ol.style.Fill({
+                    color: 'rgba(0,0,255,0.1)'
+                })
+            })];
+        }
+        return highlightStyleCache[text];
     }
-    return highlightStyleCache[text];
-  }
 });
 
 var highlight;
 var displayFeatureInfo = function(pixel) {
 
-  var feature = map.forEachFeatureAtPixel(pixel, function(feature, layer) {
-    return feature;
-  });
+    var feature = map.forEachFeatureAtPixel(pixel, function(feature, layer) {
+        return feature;
+    });
 
-  var info = document.getElementById('info');
-  if (feature) {
-    info.innerHTML = feature.get('NAME');
-    info.style.display = 'block';
-  } else {
-    info.innerHTML = '';
-    info.style.display = 'none';
-  }
-
-  if (feature !== highlight) {
-    if (highlight) {
-      featureOverlay.removeFeature(highlight);
-    }
+    var info = document.getElementById('info');
     if (feature) {
-      featureOverlay.addFeature(feature);
+        info.innerHTML = feature.get('NAME');
+        info.style.display = 'block';
+    } else {
+        info.innerHTML = '';
+        info.style.display = 'none';
     }
-    highlight = feature;
-  }
+
+    if (feature !== highlight) {
+        if (highlight) {
+            featureOverlay.removeFeature(highlight);
+        }
+        if (feature) {
+            featureOverlay.addFeature(feature);
+        }
+        highlight = feature;
+    }
 
 };
 
 $(map.getViewport()).on('mousemove', function(evt) {
-  var pixel = map.getEventPixel(evt.originalEvent);
-  displayFeatureInfo(pixel);
+    var pixel = map.getEventPixel(evt.originalEvent);
+    displayFeatureInfo(pixel);
 });
 
 // map.on('click', function(evt) {
@@ -155,27 +115,27 @@ $(map.getViewport()).on('mousemove', function(evt) {
 // });
 
 map.toggleLayer = function(layerName) {
-  if (map.activeLayers[layerName]) {
-    // then de-activate layer
-    map.removeLayer(map.activeLayers[layerName]);
-    delete map.activeLayers[layerName];    
-    $('#' + layerName).find('span').removeClass('glyphicon-check');
-    $('#' + layerName).find('span').addClass('glyphicon-unchecked');
-  } else if (map.layers[layerName]) {
-    // then activate layer
-    map.addLayer(map.layers[layerName]);
-    map.activeLayers[layerName] = map.layers[layerName];
-    $('#' + layerName).find('span').removeClass('glyphicon-unchecked');
-    $('#' + layerName).find('span').addClass('glyphicon-check');
-  }
+    if (map.activeLayers[layerName]) {
+        // then de-activate layer
+        map.removeLayer(map.activeLayers[layerName]);
+        delete map.activeLayers[layerName];    
+        $('#' + layerName).find('span').removeClass('glyphicon-check');
+        $('#' + layerName).find('span').addClass('glyphicon-unchecked');
+    } else if (map.layers[layerName]) {
+        // then activate layer
+        map.addLayer(map.layers[layerName]);
+        map.activeLayers[layerName] = map.layers[layerName];
+        $('#' + layerName).find('span').removeClass('glyphicon-unchecked');
+        $('#' + layerName).find('span').addClass('glyphicon-check');
+    }
 };
 
 map.switchBasemaps = function(basemapName) {
-  if (map.basemaps[basemapName] && map.currentBasemap !== basemapName) {
-    map.removeLayer(map.basemaps[map.currentBasemap]);
-    map.getLayers().insertAt(1, map.basemaps[basemapName]);
-    // map.layers[0] = map.basemaps[basemapName];
-    map.currentBasemap = basemapName;
-  }
+    if (map.basemaps[basemapName] && map.currentBasemap !== basemapName) {
+        map.removeLayer(map.basemaps[map.currentBasemap]);
+        map.getLayers().insertAt(1, map.basemaps[basemapName]);
+        // map.layers[0] = map.basemaps[basemapName];
+        map.currentBasemap = basemapName;
+    }
 };
 
