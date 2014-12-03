94c8c65bc4bf16a38459fab75c60d44ac4c38c81
sfletche <sfletche@gmail.com>
2014-12-02 20:25:29 -0800

    changing 'drawing' button to 'polygon'
    and preparing some of the drawing methods to handle different types of
    drawings (polygons, lines, points)

--- marco/drawing.js
+++ marco/drawing.js
@@ -27,74 +27,66 @@ var drawingVector = new ol.layer.Vector({
 // but to a feature overlay which holds a collection of features.
 // This collection is passed to the modify and also the draw
 // interaction, so that both can add or modify features.
-var featureOverlay = new ol.FeatureOverlay({
-  style: new ol.style.Style({
-    fill: new ol.style.Fill({
-      color: 'rgba(255, 255, 255, 0.2)'
-    }),
-    stroke: new ol.style.Stroke({
-      color: '#ffcc33',
-      width: 2
-    }),
-    image: new ol.style.Circle({
-      radius: 7,
-      fill: new ol.style.Fill({
-        color: '#ffcc33'
-      })
-    })
-  })
-});
-featureOverlay.setMap(map);
-
-// var modify = new ol.interaction.Modify({
-//   features: featureOverlay.getFeatures(),
-//   // the SHIFT key must be pressed to delete vertices, so
-//   // that new vertices can be drawn at the same position
-//   // of existing vertices
-//   deleteCondition: function(event) {
-//     return ol.events.condition.shiftKeyOnly(event) &&
-//         ol.events.condition.singleClick(event);
-//   }
+// var featureOverlay = new ol.FeatureOverlay({
+//   style: new ol.style.Style({
+//     fill: new ol.style.Fill({
+//       color: 'rgba(255, 255, 255, 0.2)'
+//     }),
+//     stroke: new ol.style.Stroke({
+//       color: '#ffcc33',
+//       width: 2
+//     }),
+//     image: new ol.style.Circle({
+//       radius: 7,
+//       fill: new ol.style.Fill({
+//         color: '#ffcc33'
+//       })
+//     })
+//   })
 // });
-// map.addInteraction(modify);
+// featureOverlay.setMap(map);
 
 var getDrawingLayer = function(evt) {
     return drawingVector.getSource();
 };
 
-var addNewDrawing = function(evt) {
+var addNewDrawing = function(evt, drawingType) {
   // enable the drawing buttons 
-  var drawingButton = document.getElementById('drawings');
+  var drawingButton = document.getElementById(drawingType);
   drawingButton.removeAttribute('disabled');
-  var editButton = document.getElementById('edit-drawing');
+  var editButton = document.getElementById('edit-'+drawingType);
   editButton.removeAttribute('disabled');
-  var deleteButton = document.getElementById('delete-drawing');
+  var deleteButton = document.getElementById('delete-'+drawingType);
   deleteButton.removeAttribute('disabled');
-  $('#drawings').find('span').removeClass('glyphicon-unchecked');
-  $('#drawings').find('span').addClass('glyphicon-check');  
+  $('#'+drawingType).find('span').removeClass('glyphicon-unchecked');
+  $('#'+drawingType).find('span').addClass('glyphicon-check');  
 
   // add drawing to map layers
-  map.layers['drawings'] = drawingVector;
-  drawingVector.getSource().getFeatures()[0].set('DRAWING', 'Drawing');
-  map.addLayer(map.layers['drawings']);
-  map.activeLayers['drawings'] = map.layers['drawings']; 
+  map.layers[drawingType] = drawingVector;
+  drawingVector.getSource().getFeatures()[0].set('DRAWING', drawingType);
+  map.addLayer(map.layers[drawingType]);
+  map.activeLayers[drawingType] = map.layers[drawingType]; 
 
   // end the drawing process
   map.removeInteraction(draw); 
 };
 
 var draw; // global so we can remove it later
-map.enableDrawing = function() {
+map.enableDrawing = function(drawingType) {
+  var type = "Polygon";
+  if (drawingType === 'polygon') {
+    type = "Polygon";
+  }
   draw = new ol.interaction.Draw({
     // features: featureOverlay.getFeatures(),
     source: drawingVector.getSource(),
-    type: /** @type {ol.geom.GeometryType} */ "Polygon"
+    type: /** @type {ol.geom.GeometryType} */ type
   });
   // draw.on('drawstart', function(evt) {
   //   source.clear();
   // });
   draw.on('drawend', function(evt) {
-    addNewDrawing(evt);
+    addNewDrawing(evt, drawingType);
   });
   map.addInteraction(draw);
 };
@@ -115,24 +107,34 @@ map.toggleDrawing = function(drawingName) {
     }
 };
 
-map.editDrawing = function(drawingName) {
-
+var modifyDrawing;
+map.editDrawing = function() {
+    // modifyDrawing = new ol.interaction.Modify({
+    //     features: drawingVector.getSource().getFeatures(),
+    //     // the SHIFT key must be pressed to delete vertices, so
+    //     // that new vertices can be drawn at the same position
+    //     // of existing vertices
+    //     deleteCondition: function(event) {
+    //         return ol.events.condition.shiftKeyOnly(event) && ol.events.condition.singleClick(event);
+    //     }
+    // });
+    // map.addInteraction(modifyDrawing);
 };
 
-map.deleteDrawing = function(drawingName) {
+map.deleteDrawing = function(drawingType) {
     // uncheck drawings button
-    $('#' + drawingName).find('span').removeClass('glyphicon-check');
-    $('#' + drawingName).find('span').addClass('glyphicon-unchecked');
+    $('#' + drawingType).find('span').removeClass('glyphicon-check');
+    $('#' + drawingType).find('span').addClass('glyphicon-unchecked');
     // remove drawings layer
-    map.removeLayer(map.activeLayers[drawingName]);
-    delete map.activeLayers[drawingName];   
-    map.layers[drawingName].getSource().clear(); 
-    delete map.layers[drawingName];
+    map.removeLayer(map.activeLayers[drawingType]);
+    delete map.activeLayers[drawingType];   
+    map.layers[drawingType].getSource().clear(); 
+    delete map.layers[drawingType];
     // disable drawing buttons
-    var drawingButton = document.getElementById('drawings');
+    var drawingButton = document.getElementById(drawingType);
     drawingButton.setAttribute('disabled', 'disabled');
-    var editButton = document.getElementById('edit-drawing');
+    var editButton = document.getElementById('edit-'+drawingType);
     editButton.setAttribute('disabled', 'disabled');
-    var deleteButton = document.getElementById('delete-drawing');
+    var deleteButton = document.getElementById('delete-'+drawingType);
     deleteButton.setAttribute('disabled', 'disabled');
 };
